3131528ea52a4f5981c899fbb472a75b9af471a3
PovilasLuk <povilas.lukasevicius@gmail.com>
2021-12-15 15:06:36 +0200

    Implemented navigation graph and basic drawer layout

--- app/src/main/java/com/example/touristtrips/NavigationGraphActivity.kt
+++ app/src/main/java/com/example/touristtrips/NavigationGraphActivity.kt
@@ -2,10 +2,48 @@ package com.example.touristtrips
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.Navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.navigation.NavigationView
 
 class NavigationGraphActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_navigation_graph)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        val drawerLayout = findViewById<DrawerLayout>(R.id.drawer_layout)
+
+        appBarConfiguration = AppBarConfiguration(
+            topLevelDestinationIds = setOf(
+                R.id.currentTripFragment,
+                R.id.locationsFragment,
+                R.id.routesFragment,
+                R.id.myLocationsFragment,
+                R.id.myRoutesFragment,
+                R.id.settingsFragment
+            ),
+            drawerLayout = drawerLayout
+        )
+
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        findViewById<NavigationView>(R.id.nav_view).setupWithNavController(navController)
     }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(this, R.id.nav_host_fragment)
+        return navController.navigateUp(appBarConfiguration)
+                || super.onSupportNavigateUp()
+    }
+
 }
